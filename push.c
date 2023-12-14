51b48267d788bbd5c0bdaa5e35b051b7d19beecd
Menno007 <Menna.soft3@gmail.com>
2023-12-14 03:40:33 +0200

    0

--- push.c
+++ push.c
@@ -1,15 +1,17 @@
 #include "monty.h"
 /**
  * push - adds a new node at the beginning
- * @head: pointer to pointer head
- * @n: data int
+ * @stack: pointer to pointer head
+ * @value: data int
+ * @line_number: msccw
  * Return: the address of the new element, or NULL if it failed.
  */
-void push(stack_t **head, const int n)
+void push(stack_t **stack, unsigned int line_number, int value)
 {
 	stack_t *new_node;
+	(void) line_number;
 
-	if (head == NULL)
+	if (stack == NULL)
 		exit(EXIT_FAILURE);
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
@@ -17,10 +19,10 @@ void push(stack_t **head, const int n)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = n;
+	new_node->n = value;
 	new_node->prev = NULL;
-	new_node->next = *head;
-	if (*head != NULL)
-		(*head)->prev = new_node;
-	*head = new_node;
+	new_node->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
